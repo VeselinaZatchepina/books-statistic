52469e1825c4bd8a739868d1a71e4ff51f609cfe
Veselina Zatchepina <techpro20@mail.ru>
2017-07-22 23:29:03 +0300

    feat: add implicit intent

--- BookStatistics/app/src/main/java/com/github/veselinazatchepina/bookstatistics/preference/fragments/WriteToDeveloperFragment.java
+++ BookStatistics/app/src/main/java/com/github/veselinazatchepina/bookstatistics/preference/fragments/WriteToDeveloperFragment.java
@@ -1,16 +1,30 @@
 package com.github.veselinazatchepina.bookstatistics.preference.fragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.github.veselinazatchepina.bookstatistics.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class WriteToDeveloperFragment extends Fragment {
+
+    @BindView(R.id.input_text_for_developer)
+    EditText mEditTextForInput;
+    @BindView(R.id.send_button)
+    Button sendButton;
+    private Unbinder unbinder;
 
     public WriteToDeveloperFragment() {
     }
@@ -28,6 +42,27 @@ public class WriteToDeveloperFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_write_to_developer, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputText = mEditTextForInput.getText().toString();
+                if (!inputText.equals("")) {
+                    String mail= "mailto:veselinazatchepina@gmail.com" +
+                            "?subject=" + Uri.encode("Feedback") +
+                            "&body=" + Uri.encode(inputText);
+                    Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                    emailIntent.setData(Uri.parse(mail));
+                    startActivity(Intent.createChooser(emailIntent, "Send Email"));
+                }
+            }
+        });
         return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
